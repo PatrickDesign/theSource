c9104d734f20ae1b3725b1a4a63d4aee094ad646
PatrickW <PatrickW@gmail.com>
2019-02-27 18:03:18 +0000

    Adding generic connectionString for grading

---
+++ connectionString.js
@@ -0,0 +1,3 @@
+const uri = "mongodb://patrickSource:source@cluster0-shard-00-00-28qcy.mongodb.net:27017,cluster0-shard-00-01-28qcy.mongodb.net:27017,cluster0-shard-00-02-28qcy.mongodb.net:27017/sourceDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+
+module.exports = uri;
