4e1a7dd93eefc1de3fdb4a75bbec6962ce8d2bf5
PatrickW <PatrickW@gmail.com>
2019-03-21 05:34:53 +0000

    fixing build

--- app.js
+++ app.js
@@ -517,45 +517,53 @@ app.get('/projects/:id/updates/addUpdate', (req, res) =>
 //Update a project's earnings field
 app.post('/projects/:id/acceptPayment', (req, res) =>
 {
-  
-  
+
+
 
   // req.body.donationAmount contains the amount to update project by
   Project.findById(req.params.id, (err, projectToUpdate) =>
   {
-    if(err)
-        console.log(err);
-      else{
-        if(req.body.donationAmount > 0){
-          
-          User.findById(req.user._id, (err, foundUser) =>
+    if (err)
+      console.log(err);
+    else
+    {
+      if (req.body.donationAmount > 0)
+      {
+
+        User.findById(req.user._id, (err, foundUser) =>
+        {
+          if (err)
+            console.log(err);
+          else
           {
-            if(err)
-              console.log(err);
-            else{
-              projectToUpdate.earnings = (parseInt(projectToUpdate.earnings) + parseInt(req.body.donationAmount));
-              var isInArray = projectToUpdate.backers.some((_id) => {
-                              return _id.equals(foundUser._id);
-                          });
-              if(!isInArray){
-                projectToUpdate.backers.unshift(foundUser);
-              }
-              foundUser.contributed = (parseInt(foundUser.contributed) + parseInt(req.body.donationAmount));
-              foundUser.save()
-              projectToUpdate.save((err, project) =>
+            projectToUpdate.earnings = (parseInt(projectToUpdate.earnings) + parseInt(req.body.donationAmount));
+            var isInArray = projectToUpdate.backers.some((_id) =>
+            {
+              return _id.equals(foundUser._id);
+            });
+            if (!isInArray)
+            {
+              projectToUpdate.backers.unshift(foundUser);
+            }
+            foundUser.contributed = (parseInt(foundUser.contributed) + parseInt(req.body.donationAmount));
+            foundUser.save()
+            projectToUpdate.save((err, project) =>
             {
               return res.redirect("/projects/" + projectToUpdate._id);
             });
-            }
-          });
-          
-          
-        }else{
-          return res.redirect("/projects/" + projectToUpdate._id);
+          }
+        });
+
+
+      }
+      else
+      {
+        return res.redirect("/projects/" + projectToUpdate._id);
+      }
+
     }
 
   });
-
 });
 
 
